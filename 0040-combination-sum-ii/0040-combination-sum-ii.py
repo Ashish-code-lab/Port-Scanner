23b39220b75e1701d7cd2c25641c1bd4709463a1
Ashish <ashishk16764@gmail.com>
2026-08-22 23:51:53 +0530

    Time: 5 ms (81.44%), Space: 12.4 MB (41.79%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def combinationSum2(self, candidates, target):
+        result = []
+        candidates.sort()
+
+        def backtrack(start, target, path):
+            if target == 0:
+                result.append(path[:])
+                return
+
+            for i in range(start, len(candidates)):
+
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+
+                if candidates[i] > target:
+                    break
+
+                path.append(candidates[i])
+
+                backtrack(i + 1, target - candidates[i], path)
+
+                path.pop()
+
+        backtrack(0, target, [])
+        return result
